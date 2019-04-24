e7199f53a2e8191a93a5785d79e965f45dca9f6f
aldobevindra <49948656+aldobevindra@users.noreply.github.com>
2019-04-24 20:59:30 +0800

    First Commit
    First Commit

---
+++ hubungi.php
@@ -0,0 +1,5 @@
+<center>
+	<h2>Hubungi saya di</h2><br>
+		<i class="glyphicon glyphicon-envelope"></i> 175433@smktelkommedan.sch.id<br>
+		<i class="glyphicon glyphicon-phone"></i> <a href="">081375870042</a>
+</center>
